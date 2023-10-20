7e59e56f8e2f66b3ae4813f56cd535fe2c4fc9ad
Afonso Pereira <afpereira@deloitte.pt>
2023-10-20 15:33:57 +0100

    cleaning

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -2,8 +2,6 @@ const accordionItems = document.querySelectorAll('.accordion-item');
 
 accordionItems.forEach(item => {
   const button = item.querySelector('.accordion-button');
-  const content = item.querySelector('.accordion-content');
-  const targetId = button.dataset.target;
   button.addEventListener('click', () => {
     item.classList.toggle('open');
   });

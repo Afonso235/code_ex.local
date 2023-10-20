8180389fa930bcb5db8f0d4891fddda76fbe8194
Afonso Pereira <afpereira@deloitte.pt>
2023-10-20 15:33:55 +0100

    reaarange code

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      card.classList.toggle('flipped');
-      card.classList.add('active');
-    });
-  
-    card.addEventListener('mouseover', () => {
-      card.style.transform = 'scale(1.1)';
-    });
-  
-    card.addEventListener('mouseout', () => {
-      card.style.transform = 'scale(1)';
-    });
-  });
-
-  function flipCard(card) {
+function flipCard(card) {
     console.log(card.querySelector('.back'));
     card.classList.toggle('flipped');
 }

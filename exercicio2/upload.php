d227837c66a6faa5316db8621c8f0be52ef621ac
Afonso Pereira <afpereira@deloitte.pt>
2023-10-20 15:33:55 +0100

    add extension fix

--- exercicio2/upload.php
+++ exercicio2/upload.php
@@ -1,45 +1,34 @@
 <?php
 if (isset($_POST["submit"])) {
-    $diretorio_destino = "uploads/"; 
+    $diretorio_destino = "uploads/";
 
     if (!file_exists($diretorio_destino)) {
         mkdir($diretorio_destino, 0777, true);
     }
 
     $arquivo = $_FILES["arquivo"];
-    $extensoes_permitidas = array("jpg", "jpeg", "png", "pdf"); 
-
     $extensao = pathinfo($arquivo["name"], PATHINFO_EXTENSION);
 
-    if (in_array($extensao, $extensoes_permitidas)) {
-        $nome_arquivo = $arquivo["name"];
-        $caminho_temporario = $arquivo["tmp_name"];
+    $nome_arquivo = $arquivo["name"];
+    $caminho_temporario = $arquivo["tmp_name"];
 
-        $diretorio_extensao = ($extensao === "pdf") ? "pdf" : $extensao;
+    $diretorio_extensao = $extensao;
 
-        $caminho_destino = $diretorio_destino . $diretorio_extensao . '/' . $nome_arquivo;
+    $caminho_destino = $diretorio_destino . $diretorio_extensao . '/' . $nome_arquivo;
 
-        if (!file_exists($diretorio_destino . $diretorio_extensao)) {
-            mkdir($diretorio_destino . $diretorio_extensao, 0777, true);
-        }
+    if (!file_exists($diretorio_destino . $diretorio_extensao)) {
+        mkdir($diretorio_destino . $diretorio_extensao, 0777, true);
+    }
 
-        if (move_uploaded_file($caminho_temporario, $caminho_destino)) {
-            echo "Arquivo enviado com sucesso.";
-            ?>
-            <div class="go-back">
-                <a href="index.php">Ver Lista de Ficheiros</a>
-            </div>
-            <?php        
-            } else {
-            echo "Ocorreu um erro ao enviar o arquivo.";
-            ?>
-            <div class="go-back">
-                <a href="formulario.php">Voltar para o Formulário</a>
-            </div>
-            <?php
-        }
+    if (move_uploaded_file($caminho_temporario, $caminho_destino)) {
+        echo "Arquivo enviado com sucesso.";
+        ?>
+        <div class="go-back">
+            <a href="index.php">Ver Lista de Ficheiros</a>
+        </div>
+        <?php
     } else {
-        echo "Erro: Extensão de arquivo não permitida.";
+        echo "Ocorreu um erro ao enviar o arquivo.";
         ?>
         <div class="go-back">
             <a href="formulario.php">Voltar para o Formulário</a>

68b6cc5cc3e041d09608b27aea10805cad123ea5
Afonso Pereira <afpereira@deloitte.pt>
2023-10-20 15:33:57 +0100

    init

--- exercicio2/delete.php
+++ exercicio2/delete.php
@@ -4,12 +4,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['file'])) {
     
     if (is_file($fileToDelete)) {
         unlink($fileToDelete); 
-
         $parentDir = dirname($fileToDelete);
         if (is_dir($parentDir) && count(scandir($parentDir)) === 2) {
             rmdir($parentDir);
         }
-
     }
         header('Location: index.php');
 }

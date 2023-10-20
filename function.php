cd4cb883e6a786a794fea3dc389f43499352a241
Afonso Pereira <afpereira@deloitte.pt>
2023-10-20 15:33:55 +0100

    change styles and alt name

--- function.php
+++ function.php
@@ -3,10 +3,10 @@
 function exibirCodigo($connection) {
     $tempoAtual = time();
     $intervalo = 300; // 5 minutos em segundos
-
+    
     if (!isset($_SESSION['ultima_geracao']) || ($tempoAtual - $_SESSION['ultima_geracao']) >= $intervalo) {
         $codigo = gerarCodigoAleatorio();
-
+        
         if (inserirCodigoNaBD($connection, $codigo)) {
             $_SESSION['ultima_geracao'] = $tempoAtual;
         } else {
@@ -27,7 +27,7 @@ function gerarCodigoAleatorio() {
     $codigo .= chr(random_int(97, 122));
     $codigo .= str_pad(random_int(0, 9), 2, '0', STR_PAD_LEFT);
     $codigo = str_shuffle($codigo);
-
+    
     return $codigo;
 }
 
@@ -46,17 +46,17 @@ function inserirCodigoNaBD($connection, $codigo) {
 function listarCodigos($connection) {
     $sql = "SELECT codigo, timestamp FROM code_ex ORDER BY timestamp DESC";
     $result = $connection->query($sql);
-
+    
     $imageDirectory = 'img/';
-
+    
     $imageFiles = glob($imageDirectory . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-
+    
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $randomImage = $imageFiles[array_rand($imageFiles)];
-
+            
             echo '<div class="card" onclick="flipCard(this)">';
-            echo '<img class="front" src="' . $randomImage . '" alt="Imagem do código">';
+            echo '<img class="front" src="' . $randomImage . '" alt="Imagem">';
             echo '<div class="back">';
             echo 'Código: ' . $row['codigo'] . '<br>';
             echo 'Data/Hora: ' . $row['timestamp'] . '<br>';

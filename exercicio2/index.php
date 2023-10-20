e078664588ccf8218ab503e204e4f098bf3a8b83
Afonso Pereira <afpereira@deloitte.pt>
2023-10-20 15:33:56 +0100

    removed button

--- exercicio2/index.php
+++ exercicio2/index.php
@@ -18,24 +18,6 @@
     <div class="go-back">
         <a href="formulario.php">Voltar para o Formulário</a>
     </div>
-    <div>
-        <button class="button" id="toggleButton">Toggle Mode</button>
-        <script>                  
-            const buttonElement = document.getElementById('toggleButton');
-            let isDarkMode = false;
-
-            buttonElement.addEventListener('click', () => {
-            isDarkMode = !isDarkMode;
-            if (isDarkMode) {
-                buttonElement.classList.add('dark');
-                buttonElement.classList.remove('light');
-            } else {
-                buttonElement.classList.add('light');
-                buttonElement.classList.remove('dark');
-            }
-            });
-        </script>
-    </div>
     <script src="script.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
